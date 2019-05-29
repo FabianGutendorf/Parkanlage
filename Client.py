b4896f3fc8a1007bb0452a794fb3dc090980f6ec
Moritz Bulthaup <m_bulti@Moritzs-MacBook-Pro.local>
2019-05-29 11:55:23 +0200

    Flask

--- Client.py
+++ Client.py
@@ -4,6 +4,33 @@ import sqlite3
 HOST = '127.0.0.1'
 PORT = 65432  
 
+
+class Client():
+    # Python Socket
+    def initSocket(self):
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((HOST, PORT))
+                s.sendall(b'stop')
+                data = s.recv(1024)
+
+                print('Received', repr(data))
+
+    pass
+
+
+    # Python SQLite
+    c
+
+    def __init__(self):
+        conn = sqlite3.connect('Parkanlage.db')
+        c = conn.cursor()
+        pass
+    
+    def SQLiteSelect(self, statement):
+
+        for row in c.execute(statement):
+            print(row)
+        pass
 # Python Socket Try
 
 # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -15,20 +42,9 @@ PORT = 65432
 
 
 # Python SQLite Handling
-conn = sqlite3.connect('Parkanlage.db')
-c = conn.cursor()
 
 
-for row in c.execute("SELECT * FROM Parker"):
-    print(row)
 
 
-# Python Socket
 
 
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.connect((HOST, PORT))
-#     s.sendall(b'stop')
-#     data = s.recv(1024)
-
-# print('Received', repr(data))
