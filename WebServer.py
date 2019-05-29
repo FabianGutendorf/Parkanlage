6725b9096ea164e85a76c0befe18d423182ce7e0
Moritz Bulthaup <m_bulti@Moritzs-MacBook-Pro.local>
2019-05-29 18:00:40 +0200

    merged master/dev

--- WebServer.py
+++ WebServer.py
@@ -1,11 +1,36 @@
 
-from flask import Flask
+from flask import Flask, url_for, request
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world"
+    return '<a href=' + url_for("test") + '>Zum Login</a>'
 
-@app.route("/Test")
+@app.route("/test") #HTML Formular
 def test():
-    return "Tets"
+    return '''
+        <html>
+            <body>
+                <form action = "http://localhost:4698/login" method = "post">
+                    <p>Name:</p>
+                    <p><input type = "text" name = "name" /></p>
+                    <p><input type = "submit" value = "submit" /></p>
+                </form>
+            </body>
+        </html>
+    '''
+
+@app.route("/login", methods= ['POST', 'GET'])
+def login():
+    name = ""
+    if request.method == 'POST':
+        name = request.form['name']
+    else:
+        name = request.args.GET('name')
+
+    return "logged " + name + " in"
+
+
+if __name__ == '__main__':
+    app.run(port=4698, debug=True)
