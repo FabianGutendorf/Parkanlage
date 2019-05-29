4ca2cb9158cd2c4809b6d0e27e844a6e166ebf79
Moritz Bulthaup <m_bulti@Moritzs-MacBook-Pro.local>
2019-05-29 10:42:02 +0200

    Python SQLite

--- Client.py
+++ Client.py
@@ -4,6 +4,8 @@ import sqlite3
 HOST = '127.0.0.1'
 PORT = 65432  
 
+# Python Socket Try
+
 # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 #     s.connect((HOST, PORT))
 #     s.sendall(b'Hello, world')
@@ -12,10 +14,21 @@ PORT = 65432
 # print('Received', repr(data))
 
 
+# Python SQLite Handling
+conn = sqlite3.connect('Parkanlage.db')
+c = conn.cursor()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'stop')
-    data = s.recv(1024)
 
-print('Received', repr(data))
+for row in c.execute("SELECT * FROM Parker"):
+    print(row)
+
+
+# Python Socket
+
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((HOST, PORT))
+#     s.sendall(b'stop')
+#     data = s.recv(1024)
+
+# print('Received', repr(data))
